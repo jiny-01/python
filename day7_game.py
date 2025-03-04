f6441ec7a4dde2ccf2e7c2f3264afc87dfb4fc5c
allie7019@naver.com <allie7019@naver.com>
2025-03-04 21:28:00 +0900

    Feat:day7

--- day7_game.py
+++ day7_game.py
@@ -7,6 +7,13 @@ user_score = 0
 computer_score = 0
 
 while True:
+    if user_score == 5:
+        print("사용자 승리~!!!!")
+        break
+    elif computer_score == 5:
+        print("컴퓨터 승리~!!!!")
+        break
+
     user_choice = input("가위, 바위, 보 중 선택해 주세요(그만하려면 종료) : ")
     if user_choice == "종료":
         print(f"종료되었습니다. 컴퓨터 - {computer_score} 사용자 - {user_score}")
@@ -19,12 +26,15 @@ while True:
 
     if user_choice == computer_choice:
         print("무승부 입니다.")
-        continue
-
     elif (user_choice == "가위" and computer_choice == "보") or\
           (user_choice == "바위" and computer_choice == "가위") or\
           (user_choice == "보" and computer_choice == "가위"):
     #행 늘일 땐 엔터만 하면 오류, 전체를 () 묶거나 \ 이용
         print("이겼습니다!!!")
+        user_score += 1
     else:
         print("졌습니다 ㅜㅜ")
+        computer_score += 1
+
+    print(f"컴퓨터 - {computer_score} 사용자 - {user_score}")
+    continue
